a5360cb327b510b24e7b66879c2e0c4c11ba1259
houchengyue <houchengyue@bwic.cn>
2023-04-28 17:27:35 +0800

    feature(master): 选中文件路径显示在输入框

--- src/main/kotlin/com/hcy/androidlocalizationbyform/translation/ShowDialog.kt
+++ src/main/kotlin/com/hcy/androidlocalizationbyform/translation/ShowDialog.kt
@@ -1,5 +1,6 @@
 package com.hcy.androidlocalizationbyform.translation
 
+import com.hcy.androidlocalizationbyform.translation.dialog.ContentDialog
 import com.hcy.androidlocalizationbyform.translation.dialog.TestDialog
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -7,7 +8,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 class ShowDialog : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
 //        ContentDialog().show()
-        val dialog = TestDialog()
+        val dialog = ContentDialog()
         dialog.pack()
         dialog.isVisible = true
     }
--- src/main/kotlin/com/hcy/androidlocalizationbyform/translation/dialog/ContentDialog.kt
+++ src/main/kotlin/com/hcy/androidlocalizationbyform/translation/dialog/ContentDialog.kt
@@ -1,44 +1,81 @@
 package com.hcy.androidlocalizationbyform.translation.dialog
 
+import com.fasterxml.aalto.util.TextUtil
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import java.awt.event.KeyEvent
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import java.util.Objects
 import javax.swing.*
 
 /**
  * 主体窗口
  */
-class ContentDialog(xmlFilePath: String="") : JDialog() {
+class ContentDialog(xmlFilePath: String = "") : JDialog() {
     private lateinit var root: JPanel
     private lateinit var title: JLabel
     private lateinit var xmlPath: JTextField
-    private lateinit var chooseXml: JButton
+    private lateinit var btnChooseXml: JButton
     private lateinit var xlxFilePath: JTextField
-    private lateinit var chooseXlxs: JButton
-    private lateinit var summit: JButton
+    private lateinit var btnChooseXlxs: JButton
+    private lateinit var btnClose: JButton
+    private lateinit var btnSummit: JButton
     private lateinit var log: JTextArea
 
 
     init {
-        xmlPath.text=xmlFilePath
+        xmlPath.text = xmlFilePath
         setContentPane(root)
-        isModal=true
-        getRootPane().defaultButton=chooseXml
-        chooseXml.addActionListener { e->
+        isModal = true
+        if (xmlFilePath.isEmpty()) {
+            getRootPane().defaultButton = btnChooseXml
+        } else {
+            getRootPane().defaultButton = btnChooseXlxs
+        }
+        btnChooseXml.addActionListener { e ->
+            val descriptor = FileChooserDescriptor(true, false, false, true, false, true)
+            "请选择strings 下的Xml文件！".also { descriptor.description = it }
+            val file = FileChooser.chooseFile(descriptor, null, null)
+            xmlPath.text= requireNotNull(file).path
+        }
 
-//            val descriptor = FileChooserDescriptor(true, false, false, true, false, true)
-//            "请选择翻译表格文件！".also { descriptor.description = it }
-//            val chooseFile = FileChooser.chooseFiles(descriptor, e.project, null)
-//            for (file in chooseFile) {
-//                println("name = ${file.name}")
-//                println("path = ${file.path}")
-//            }
+        btnChooseXlxs.addActionListener {
+            val descriptor = FileChooserDescriptor(true, false, false, true, false, true)
+            "请选择表格文件！（xlx、xlxs）".also { descriptor.description = it }
+            val file = FileChooser.chooseFile(descriptor, null, null)
+            xlxFilePath.text= requireNotNull(file).path
+        }
+
+        btnSummit.addActionListener {
+
         }
 
 
+        btnClose.addActionListener {
+            dispose()
+        }
+
+        defaultCloseOperation = DO_NOTHING_ON_CLOSE
+
+        addWindowListener(object : WindowAdapter() {
+            override fun windowClosed(e: WindowEvent?) {
+                super.windowClosed(e)
+                onCancel()
+            }
+        })
+
+        root.registerKeyboardAction(
+            { onCancel() },
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+            JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
+        )
 
 
+    }
 
-
+    private fun onCancel() {
+        dispose()
     }
 
 
